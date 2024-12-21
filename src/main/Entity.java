b88a04ce4f2957a0c83ab70b10a3833cb794cbad
dannyv7813 <dannyv7813@gmail.com>
2024-12-20 20:28:29 -0500

    increased Dmg function, entity :l
    attack function , block function , entity

--- src/main/Entity.java
+++ src/main/Entity.java
@@ -4,22 +4,49 @@ public abstract class Entity {
 
     protected int hp;
     protected int dmg;
+    protected int def;
+
 
     //Default Entity
-    public Entity(){
+    public Entity() {
         hp = 100;
         dmg = 10;
+        def = 5;
     }
-    public Entity(int HP,int DMG){
+
+    public Entity(int HP, int DMG, int DEF) {
         hp = HP;
         dmg = DMG;
+        def = DEF;
     }
 
 
-    public void increasedDmg(int add){
+    public void takeDamage(int damage) {
+       hp -= damage;
+    }
+
+    public void attack(Entity target) {
+            target.takeDamage(dmg);
+    }
+
+
+    public void block(int damage){
+        if (damage <= def){
+            def -= damage;
+        } else {
+            int leftoverDamage = damage - def;
+            def = 0;
+            hp -= leftoverDamage;
+        }
+    }
+
+
+    public void increasedDmg(int add) {
         dmg += add;
     }
 
-    public int getHealth(){return hp;}
+    public int getHealth() {
+        return hp;
+    }
 
 }
